e2e3f01534c119f0c20e3d63bec95d15aa5724f0
Patrick Jhonatan Hernandez Blanco <patrickjhonatanh@gmail.com>
2025-11-13 12:58:35 -0600

    Console interface for library

--- biblioteca.py
+++ biblioteca.py
@@ -1,4 +1,5 @@
 from exceptions import UsuarioNoEncontradoError
+from libros import LibroDigital, LibroFisico
 
 
 class Biblioteca:
@@ -19,3 +20,12 @@ class Biblioteca:
 			if usuario.curp == curp:
 				return usuario
 		raise UsuarioNoEncontradoError(f"El usuario con CURP {curp} no fue encontrado en la biblioteca.")
+	
+	def agregar_libro(self, tipo_libro, titulo, autor, isbn):
+		if tipo_libro == "fisico":
+			nuevo_libro = LibroFisico(titulo, autor, isbn)
+		else:
+			nuevo_libro = LibroDigital(titulo, autor, isbn)
+		
+		self.libros.append(nuevo_libro)
+		return f"El libro {titulo} ha sido agregado a la biblioteca."
